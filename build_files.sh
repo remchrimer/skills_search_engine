2bbaa9c52bad8e17e4b13c640312537f1085e283
David Lee <82908669+remchrimer@users.noreply.github.com>
2024-08-26 17:39:08 -0400

    added requirements 3

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,7 @@ echo "BUILD START"
 python3.12 -m venv venv
 source venv/bin/activate
 pip install django
+pip install distutils
 pip install -r requirements.txt
 python3.12 manage.py migrate
 python3.12 manage.py collectstatic
