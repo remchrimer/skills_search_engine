9ae06d4d8dc211e94536fc823de1102b3b7a65c2
David Lee <82908669+remchrimer@users.noreply.github.com>
2024-08-26 16:25:45 -0400

    modified settings.py

--- skills_search_engine/settings.py
+++ skills_search_engine/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
