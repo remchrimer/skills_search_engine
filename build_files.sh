b05b759557a1efa6fdf4d9df7230967783f81b2f
David Lee <82908669+remchrimer@users.noreply.github.com>
2024-08-26 20:07:55 -0400

    added requirements 13

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,9 @@ echo "BUILD START"
 python3.9 -m venv venv
 source venv/bin/activate
 pip install django
+echo "TESTING 1"
+pip install --upgrade setuptools
+echo "TESTING 2"
 pip install -r requirements.txt
 python3.9 manage.py migrate
 python3.9 manage.py collectstatic
